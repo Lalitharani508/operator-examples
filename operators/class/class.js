6b76a8b868601e5d0eac132aa5198cb46d0c450f
lalitharani508 <lalithad260@gmail.com>
2024-11-30 22:08:31 +0530

    operators 2

---
+++ operators/class/class.js
@@ -0,0 +1,212 @@
+// if(false){
+//     console.log("hello true");
+// }
+// else{
+//     console.log("hello flase");
+// }
+
+// var a={
+//     id:1,
+//     name:"lalli",
+//     age:24,
+// }
+// console.log(a);
+// console.log(a.length);
+// console.log(a.name);
+
+// var a=10;
+// var b=20;
+// var c=true;
+// var d=false;
+// console.log(a+b);
+// console.log(c+d);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b); //modulus -- remainder
+// console.log(true % false);
+// console.log(true / false);
+
+
+// conditional statements---
+//{} block 
+// if(false){
+//     console.log("true ")
+// }
+// else{
+//     console.log("false")
+// }
+// if(true/false){
+//     console.log("hello true");
+// }
+// else{
+//     console.log("hello false");
+// }
+
+// if(1){ //true-1
+//     console.log("hello im true")
+
+// }else{
+//     console.log("hello im false")
+// }
+// if(100){
+//     console.log("iam true")
+// }
+
+// else{
+//     console.log("iam false")
+// }
+// if(-100){ //true
+//     console.log("iam true")
+// }
+
+// else{
+//     console.log("iam false")
+// }
+
+
+// if([]){ //true
+//     console.log("iam true");
+// }
+// else{
+//     console.log("iam false");
+// }
+// if(undefined){ //flase
+//     console.log("iam true");
+// }else{
+//     console.log("im false");
+// }
+// if({}){ //true
+//     console.log("iam true");
+// }
+// else{
+//     console.log("iam false");
+// };
+
+// var c=undefined;
+// if(c=10){
+//     console.log("iam true");
+// }
+// else{
+//     console.log("iam false");
+// }
+// var c=[];
+// if(c[0]="lalli"){
+//     console.log("iam trueee");
+// }
+// else{
+//     console.log("iam false");
+// }
+
+// [] ? console.log("truth value"): console.log("false value");
+// a=10;
+// b="lalli";
+// (a!=b) ?console.log("truth value"): console.log("false value");
+a=100;
+b=200;
+a>b? console.log(a+"is greater") : console.log(b+ "is greater");
+
+
+//operators 2
+//assigment
+var a=10;
+a=20;
+a+=20
+console.log(a);
+
+var b=10;
+b-=10;
+console.log(b);
+
+var c=200;
+c*=1;
+c=c*2
+console.log(c);
+
+var d=40;
+d/=4;
+d=d-10
+console.log(d);
+
+var e=55;
+e/=11;
+console.log(e);
+
+var o=12;
+o%=84;
+console.log(o);
+
+var l=[1,2,3];
+l[1]="lalli";
+l[3]=90;
+l[10]="p"
+console.log(l);
+console.log(l.length);
+console.log(l.length-1);
+
+var aa={
+    id:1,
+    name:"lalli",
+    age:21,
+}
+console.log(aa);
+
+var k=10;
+var r=20;
+console.log(k && r);
+console.log(a>b && a<b);
+console.log(a<b && a>b);
+console.log(false && true);
+console.log(true && false);
+
+var pp=10;
+var kk=20;
+console.log(a>b || a<b);
+console.log();
+console.log("hi");
+
+var x=10;
+var y=0;
+var z=10;
+console.log(10<10);
+console.log(x>y || x>y);
+console.log(x<y || x<z || z>y); //10<0---f
+                                //10<10--
+
+var m=23;
+console.log(!!m); //23-->t,f,t
+
+if(!!"lalli"){
+    console.log("yes");
+    
+}
+else{
+    console.log('no');
+    
+}
+if(10 && 10){
+    console.log("yeah");
+}else{
+    console.log("nah");
+}
+
+if(["karthik"]==[1,2,3]){
+    console.log("hello good morning");
+}else{
+    console.log("hi nice to meet you");
+}
+
+if(["l","akd"] && undefined){
+    console.log("evening");
+    
+}else{
+    console.log("morning");
+    
+}
+if(null && null){
+    console.log("yess");
+}else{
+    console.log("noo");
+    
+}
+
+
