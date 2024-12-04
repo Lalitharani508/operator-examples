afda64634aa0481b82dfa2120a2e0f80c6a2b6a8
lalitharani508 <lalithad260@gmail.com>
2024-12-04 19:54:06 +0530

    arraymethods

---
+++ arraymethods/arr.js
@@ -0,0 +1,228 @@
+// var arr=["flipkart","amazon"];
+// arr.push()
+// console.log(arr);
+// arr.unshift("libas","purple","meesho","ajio","myntra")
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.length-1);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// console.log(arr.reverse());
+// console.log(arr.join());//, vasthai
+// console.log(arr.join(" "));//no commas
+// console.log(arr.join("")); //space
+// console.log(arr.includes("libas"));
+// console.log(arr.includes("meesho"));
+// var a=["a","b","c"]
+// console.log(arr.concat(a).concat("dfg"));
+// // console.log(arr.split(" "));
+// console.log(arr.indexOf("ajio"));
+// // var t=arr.length();
+// console.log(arr.length);
+// console.log(arr.slice(1,6));
+// console.log(arr);
+// // console.log();
+
+
+      
+
+
+// TASK - 1 
+var a=[501,502,504,507,508];
+console.log(a.length);
+console.log(a.push("hi"));
+console.log(a);
+console.log(a.pop());
+console.log(a);
+console.log(a.shift());
+console.log(a);
+console.log(a.reverse());
+console.log(a.indexOf(504));
+console.log(a.includes(504));
+console.log(a.sort());
+console.log(a.join());
+// Questions with Multiple Array Methods:
+//1
+var c=[3,4244,6532,43.151,565,"rgt","trgtr"]
+console.log(c);
+console.log(c.push("kalki","rrr","bahubali","maryadaramanna"));
+console.log(c);
+console.log(c.shift());
+console.log(c);
+//2
+var t=["kalki","darling","mirchi","bahubali","spirit"];
+console.log(t)
+
+console.log(t.reverse().join());
+//3
+console.log(t.sort())
+t.pop()
+console.log(t);
+//4
+var h=[321,454,"lalli",56]
+var ba=["cse","ece"]
+var d=h.concat(ba);
+console.log(d);
+console.log(h.sort());
+console.log(h.pop());
+console.log(h);
+
+// Question 1: Manage a Guest List
+var j=["Alice", "Bob", "Charlie", "David"]
+console.log(j);
+console.log(j.unshift("tom"));
+console.log(j);
+console.log(j.pop());
+console.log(j.includes("Bob"));
+console.log(j)
+console.log(a.includes(504));
+// Question 2: Generate a Sentence
+// var arr=[ "Learn","Practice","Improve"]
+var l= "Learn,Practice,Improve"
+// console.log(l.join());
+// console.log(typeof l.join());
+
+// console.log(typeof l);
+// console.log(l.split());
+// console.log(l);
+
+console.log(l.split("").reverse().join(" "));
+// console.log();
+
+// Question 3: Create a Playlist
+var x=["Song1", "Song2", "Song3"];
+var y=["Song4", "Song5"];
+n=x.concat(y)
+console.log(n);
+s=n.shift()
+console.log(s)
+console.log(n);
+
+v=n.push("Song 6")
+console.log(v);
+
+console.log(n);
+
+//Question 4/5: Extract Favorite Movies
+
+var A=["Inception", "Titanic", "Avatar", "Interstellar", "Gladiator"];
+console.log(A);
+C=A.slice(1,4)
+console.log(C);
+var r=["The Matrix", "The Godfather"];
+g= C.concat(r)
+
+console.log(g);
+console.log(g.reverse());
+
+// Question 7: String Operations on Names
+
+var o="John,Doe,Jane,Smith";
+L=o.split(",");
+console.log(o);
+console.log(L);
+console.log(L.includes("jane"));
+B=L.reverse()
+console.log(B);
+S=B.join(" ")
+
+console.log(S);
+// // Question 8: Length-Based Manipulation
+
+// var f=[1, 2, 3, 4, 5];
+// console.log(f);
+// console.log(f.shift());
+// console.log(f.push(6,7));
+// console.log(f);
+
+// console.log(f.length);
+// console.log(f);
+// Question 9: Shopping Cart Operations
+// var item=["Milk", "Bread", "Eggs", "Butter"];
+// console.log(item.includes("Eggs"));
+// z=item.pop()
+// console.log(z);
+// console.log(item);
+// e=item.push("Cheese","Juice");
+// console.log(e);
+// console.log(item);
+
+
+// // Question 10: Rearrange and Combine Names
+// // var D=["Alice", "Bob", "Charlie"] 
+// // var R= ["David", "Eve"];
+// // console.log(D);
+
+// // var F=D.reverse()
+// // console.log(F);
+// // O=F.concat(R);
+// // console.log(O);
+// // console.log(O.unshift("Frank"));
+// // console.log(O);
+// var D=["Alice", "Bob", "Charlie"] 
+// Q=D.reverse()
+// console.log(Q.);
+
+
+
+// //Question 11: Shopping Cart Operations
+// console.log("Question 11");
+
+// var M=["Milk", "Bread", "Eggs", "Butter"];
+// console.log(M);
+//  M[2]="chesee"
+// console.log(M.indexOf("Eggs"));
+// M[2]="Cheese"
+// // console.log(X);
+// console.log(M);
+
+// H=M.unshift("Juice")
+// console.log(H);
+// console.log(M);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
