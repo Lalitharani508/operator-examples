e873a71dcb23b7c9027fb2a3f98fa1e7a724b10f
lalitharani508 <lalithad260@gmail.com>
2024-12-11 23:11:46 +0530

    task with simple examples and function eg explanation

---
+++ whileloop/while.js
@@ -0,0 +1,131 @@
+// while(condition){
+//     //code to execute
+//     //increment/decrement
+// }
+//printinting numbers fro 0 - 10
+// a=0;
+// while(a<=10){
+//     console.log(a);
+//     a++
+// }
+
+//printing numbers from 10 -0
+// h=10;
+// while(h>=1){
+//     console.log(h);
+//     h--;
+// }
+// a=10;
+// while(a>=1){
+//     // console.log(a);
+//     a--;
+// }
+// console.log(a);
+// a=1
+// while(a<=11){
+//     a++
+// }
+// console.log(a); //11//12
+
+// a=2;
+// while(a<=5){
+//     console.log(a)
+//     a++;
+// }
+
+//printing numbers from -2 to-5
+// a=-2
+// while(a>=-5){
+//     console.log(a);
+//     a--;
+    
+// }
+// a=-1
+// while(a>=-5){
+//     a--;
+    
+// }
+// console.log(a);//-6
+//printing even numbers from given array
+// var f=[1,2,3,4,56,7,76]
+// var i=0;
+// while(i<f.length){
+//     if(f[i]%2==0){
+//         console.log(f[i]);
+        
+//     }
+//     i++
+// }
+
+console.log("problem sloving");
+//1-10
+// a=1
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
+//10-0
+// a=10;
+// while(a>=0){
+//     console.log(a);
+//     a--;
+    
+// }
+//-1 t -10
+// a=-1;
+// while(a>=-10){
+//     console.log(a)
+//     a--;
+// }
+
+//-10 to -1
+// let v=prompt("enter number:")
+// while(v<=-1){
+//     console.log(v)
+//     v++
+// }
+
+// Print even nu/mbers and odd numbers using while loop and take userInput 
+
+// var start=1
+// var end=10
+// var a=start
+// while(a<=end){
+//     if(a%2==0){
+//         console.log(a)
+//     }
+//     a++
+// }
+
+
+// odd numbers
+// var start=1
+// var end=15
+// a=start
+// while(a<=end){
+//     if(a%2!=0)
+//     {
+//         console.log(a)
+//     }
+//     a++
+// }
+
+// print sum of even and sum of odd numbers from userInput
+
+var sumeven=0
+var sumodd=0
+var start=1
+var end=10
+var a=start
+while(a<=end){
+    if(a%2==0){
+        sumeven+=a
+    }else{
+        sumodd+=a
+    }
+    a++
+}
+console.log(sumeven)
+console.log(sumodd)
+
+
